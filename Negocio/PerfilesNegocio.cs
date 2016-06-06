e5fd99a13014a4411280253b6d29746d3d1c9ab2
abrahamfrayde <abraham.frayde@gmail.com>
2016-06-05 20:10:14 -0500

    Gestion de Perfiles

--- Negocio/PerfilesNegocio.cs
+++ Negocio/PerfilesNegocio.cs
@@ -10,12 +10,66 @@ namespace Negocio
 {
     public class PerfilesNegocio
     {
-        public List<Perfiles> list()
+        PerfilesDAL _perfilesDAL;
+        public int insertarPerfil(Perfiles _objperfil, List<Menu> _lstmenu)
         {
             try
             {
-                PerfilesDAL _perfilesDAL = new PerfilesDAL();
-                return _perfilesDAL.obtenerPerfiles();
+                _perfilesDAL = new PerfilesDAL();
+                return _perfilesDAL.insertarPerfil(_objperfil,_lstmenu);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public void modificarPerfil(Perfiles _objperfil)
+        {
+            try
+            {
+
+                _perfilesDAL = new PerfilesDAL();
+                _perfilesDAL.modificarPerfil(_objperfil);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public void eliminarPerfil(Perfiles _objperfil)
+        {
+            try
+            {
+
+                _perfilesDAL = new PerfilesDAL();
+                _perfilesDAL.eliminarPerfil(_objperfil);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Perfiles> list(int id=0,string filtro=null)
+        {
+            try
+            {
+
+                _perfilesDAL = new PerfilesDAL();
+                return _perfilesDAL.obtenerPerfiles(id,filtro);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public List<Menu> listmenus(int id=0)
+        {
+            try
+            {
+
+                _perfilesDAL = new PerfilesDAL();
+                return _perfilesDAL.obtenerMenus(id);
             }
             catch (Exception ex)
             {
