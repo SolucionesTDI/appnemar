8af47d38465141bab59f430c6c8f4e87bd6976ab
abrahamfrayde <abraham.frayde@gmail.com>
2016-06-07 11:54:46 -0500

    Pagina de Inicio

--- WebRetros/Inicio.aspx.cs
+++ WebRetros/Inicio.aspx.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entidades;
+using Negocio;
 
 public partial class Inicio : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        MenusBL bl = new MenusBL();
+        UsuariosBL userbl = new UsuariosBL();
+        if (!IsPostBack)
+        {
+
+            List<UsuariosDatos> _lstusuariodatos = new List<UsuariosDatos>();
+            _lstusuariodatos = userbl.list(0, 0, 0, 0, (int)Session["IdUser"]);
+            if (_lstusuariodatos.Count == 0)
+            {
+                nombrecompleto.Text = "Soporte";
+                lblSede.Text = "No Aplica";
+                lblDepartamento.Text = "No Aplica";
+                lblPuesto.Text = "No Aplica";
+            }
+            else
+            {
+                nombrecompleto.Text = _lstusuariodatos[0].NombreCompleto;
+                lblSede.Text = _lstusuariodatos[0].ObjSedes.descripcion;
+                lblDepartamento.Text = _lstusuariodatos[0].ObjDepto.descripcion;
+                lblPuesto.Text = _lstusuariodatos[0].ObjPuestos.descripcion;
+            }
+
+        
+        }
+
 
     }
 }
